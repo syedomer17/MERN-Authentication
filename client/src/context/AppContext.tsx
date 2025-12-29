78241d2ca053558f66a452232baf6714c1b6d224
syedomer17 <syedomerali2006@gmail.com>
2025-12-29 21:28:50 +0530

    Updated by Omer on 2025-12-29 at 09:28PM

---
+++ client/src/context/AppContext.tsx
@@ -0,0 +1,82 @@
+import axios from "axios";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import type { ReactNode } from "react";
+import { server } from "../main";
+
+export interface User {
+  _id: string;
+  name: string;
+  email: string;
+}
+
+interface AppContextType {
+  user: User | null;
+  loading: boolean;
+  isAuth: boolean;
+  setIsAuth: React.Dispatch<React.SetStateAction<boolean>>;
+  fetchUser: () => Promise<void>;
+}
+
+interface AppProviderProps {
+  children: ReactNode;
+}
+
+const AppContext = createContext<AppContextType | undefined>(undefined);
+
+export const AppProvider = ({ children }: AppProviderProps) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [isAuth, setIsAuth] = useState(false);
+
+  const fetchUser = async (): Promise<void> => {
+    setLoading(true);
+    try {
+      const { data } = await axios.get<User>(
+        `${server}/api/v1/me`,
+        { withCredentials: true }
+      );
+
+      setUser(data);
+      setIsAuth(true);
+    } catch {
+      setUser(null);
+      setIsAuth(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  return (
+    <AppContext.Provider
+      value={{
+        user,
+        loading,
+        isAuth,
+        setIsAuth,
+        fetchUser,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+
+export const useAppData = (): AppContextType => {
+  const context = useContext(AppContext);
+
+  if (!context) {
+    throw new Error("useAppData must be used within AppProvider");
+  }
+
+  return context;
+};
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { AppProvider } from "./context/AppContext.tsx";
 
-export const server = 'http://localhost:5000';
+export const server = "http://localhost:5000";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </>
+);
