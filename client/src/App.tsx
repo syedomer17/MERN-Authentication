ebdb1ae08bedc604c9282f61bcf6b2b8137ca6d0
syedomer17 <syedomerali2006@gmail.com>
2025-12-29 21:59:19 +0530

    Updated by Omer on 2025-12-29 at 09:59PM

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,37 @@
-// import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
 import Home from "./components/pages/Home";
 import Login from "./components/pages/Login";
 import Register from "./components/pages/Register";
 import Verify from "./components/pages/Verify";
 import VerifyOtp from "./components/pages/VerifyOtp";
-import { ToastContainer } from "react-toastify";
+import Loading from "./Loading";
+
+import { useAppData } from "./context/AppContext";
 
 const App = () => {
+  const { isAuth, loading } = useAppData();
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/verify" element={<Verify />} />
-          <Route path="/verify-otp" element={<VerifyOtp />} />
-        </Routes>
-        <ToastContainer />
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={isAuth ? <Home /> : <Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/verify" element={<Verify />} />
+        <Route
+          path="/verify-otp"
+          element={isAuth ? <VerifyOtp /> : <Login />}
+        />
+      </Routes>
+
+      <ToastContainer />
+    </BrowserRouter>
   );
 };
 
